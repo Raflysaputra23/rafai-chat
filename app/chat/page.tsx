db79113962d948072d71e169346abd5385e31483
Rafly Saputra <966raflisaputra@gmail.com>
2026-02-20 23:04:59 +0700

    memperbaiki url

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -82,7 +82,7 @@ const Chatbot = () => {
         formdata.append("typeChat", "multimodal");
       }
 
-      const res = await fetch("http://localhost:3000/api/v1", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_URL_DOMAIN}/api/v1`, {
         method: "POST",
         headers: {
           Authorization: "Bearer " + process.env.NEXT_PUBLIC_API_KEY
